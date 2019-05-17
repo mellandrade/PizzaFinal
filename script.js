8d998d5a84398537d57da51ed7b27ab214490f9a
Mell <30361727+mellandrade@users.noreply.github.com>
2019-05-16 20:25:28 -0700

    Add files via upload
    updated

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 /*eslint-env browser*/
 
-$(document).ready(function(){
-  $(".dropdown-toggle").dropdown();
-});
+function myFunction() {
+    "use strict";
+    window.document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) {
+    "use strict";
+    if (!event.target.matches('.dropbtn')) {
+        var i, dropdowns = document.getElementsByClassName("dropdown-content");
+        for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
