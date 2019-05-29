4db42778dde71e5763e513ac56df810062686445
Mell <30361727+mellandrade@users.noreply.github.com>
2019-05-28 21:14:36 -0700

    Add files via upload
    update

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ window.onclick = function (event) {
     "use strict";
     if (!event.target.matches('.dropbtn')) {
         var i, dropdowns = document.getElementsByClassName("dropdown-content");
-        for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+        for (i = 0; i < dropdowns.length; i += 1) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
+};
+function showField(name) {
+    "use strict";
+    if (name === "Other")document.getElementById("textbox").innerHTML = "Other: <input type="text" name="other"/>";
+    else document.getElementById("textbox").innerHTML="";
 }
